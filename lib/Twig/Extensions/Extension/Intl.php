b8d8da47261dba557d3bbb51876694439ad55262
Frank Liepert <trainmaster@gmx.net>
2014-03-29 12:59:08 +0100

    Fix: maintain copyright

--- lib/Twig/Extensions/Extension/Intl.php
+++ lib/Twig/Extensions/Extension/Intl.php
@@ -1,4 +1,13 @@
 <?php
+
+/*
+ * This file is part of Twig.
+ *
+ * (c) 2010 Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 class Twig_Extensions_Extension_Intl extends Twig_Extension
 {
     public function __construct()
